4f694dc7831dbf5cee603f0437dc1743e5ae92ee
Michael Thaler <michael.thaler@dlr.de>
2021-07-05 16:38:19 +0200

    Updated ValidatedExtensions.kt.

--- src/main/kotlin/com/mthaler/validation/functional/ValidatedExtensions.kt
+++ src/main/kotlin/com/mthaler/validation/functional/ValidatedExtensions.kt
@@ -2,7 +2,7 @@ package com.mthaler.validation.functional
 
 import arrow.core.Validated
 
-fun <E, A, B>Validated<List<E>, A>.andThen(f: (A) -> Validated<List<E>, B>): Validated<List<E>, B> = when(this) {
+fun <E, A, B>Validated<E, A>.andThen(f: (A) -> Validated<E, B>): Validated<E, B> = when(this) {
     is Validated.Invalid -> this
     is Validated.Valid -> f(this.value)
 }
