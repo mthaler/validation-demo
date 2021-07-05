90355f574ed9c6117a78d5eed70d56eaf5bcd320
Michael Thaler <michael.thaler@dlr.de>
2021-07-05 09:47:15 +0200

    Added hibernate validator dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,10 @@ dependencies {
 	implementation("org.slf4j:slf4j-api:1.7.31")
 	implementation("com.google.guava:guava:30.1.1-jre")
 	implementation("ch.qos.logback:logback-classic:1.2.3")
+    // Hibernate validator...
+	implementation("org.hibernate:hibernate-validator:6.0.13.Final")
+	implementation("org.glassfish:javax.el:3.0.0")
+	implementation("javax.validation:validation-api:2.0.1.Final")
 	testImplementation("io.kotest:kotest-runner-junit5:4.3.2")
 	testImplementation("io.kotest:kotest-assertions-core:4.3.2")
 }
