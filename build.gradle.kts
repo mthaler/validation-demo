667ce6206a8c815b4a5a21304e15fd3575c91a33
Michael Thaler <michael.thaler@dlr.de>
2021-07-05 09:17:23 +0200

    Added build.gradle.kts and settings.gradle.kts.

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	kotlin("jvm") version "1.5.10"
+}
+
+group = "com.mthaler"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("io.arrow-kt:arrow-core:0.13.2")
+	testImplementation("io.kotest:kotest-runner-junit5:4.3.2")
+	testImplementation("io.kotest:kotest-assertions-core:4.3.2")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "1.8"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
